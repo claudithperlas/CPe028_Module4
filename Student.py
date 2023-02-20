e4c7d38dd17e38484971362796ec0287f1bf0006
claudithperlas <claudithperlas9@gmail.com>
2023-02-20 09:35:28 +0000

    Add Student.py file to CPe028_Module4

---
+++ Student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+p1 = Student("Claudith Perlas", 21)
+p2 = Student("Miguel Reyes", 55)
+print(p1)
+print("Name:" p1.name)
+print("Name:" p1.age)
+print(p2.name)
+print("Name:" p2.name)
+print("Name:" p2.age)
+print(p2.name)
