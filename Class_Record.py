597a55d49acfcd1bf92309c5bbd7b7a5cba63197
claudithperlas <claudithperlas9@gmail.com>
2023-02-20 10:25:40 +0000

    Add Class_Record.py file to CPe028_Module4

--- Class_Record.py
+++ Class_Record.py
@@ -1,6 +1,7 @@
-import Student
+import Grades
 
-p1=Student.Student("Claudith Perlas", 21)
+p1=Grades.Grades("CPE028", 4, 1.75)
 print(p1)
-print("Name: ", p1.name)
-print("Age: ", p1.age)
+print("Course Code: ", p1.code)
+print("Course Unit: ", p1.unit)
+print("Course Grade: ", p1.grade)
